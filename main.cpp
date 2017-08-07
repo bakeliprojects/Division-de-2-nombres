d7ade37a1d681e2bf2a7477f7cf1e4236195045c
tawfeh <sennegueye@gmail.com>
2017-08-07 09:37:25 +0000

    Add All files

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+double a,b(0),c ;
+
+    cout << "saisir un nombre " << endl;
+     cin >> a;
+     while (b==0){
+     cout << "saisir un autre nombre " << endl;
+     cin >> b;
+     }
+
+     c= a/b;
+    cout << a << " * " << b << " = "<< c <<endl;
+       return 0;
+
+}
+
+
